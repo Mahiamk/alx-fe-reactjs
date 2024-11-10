50fd9795e3bda55105e5b42c715b5979a4f8bb8f
Anwar Koji <camk0966@gmail.com>
2024-11-11 00:46:29 +0800

    Rector prop drilling to use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ProfilePage from './components/ProfilePage'
+import React from 'react';
+import ProfilePage from './components/ProfilePage';
 import UserContext from './UserContext';
+
 function App() {
-  const userData = { name: "Jones Doe", email: "Jones.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
     <UserContext.Provider value={userData}>
-       <ProfilePage />
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
 
 export default App;
-
-
