289b8bebe08e9877ec64cb13237f145b772b4a4f
Anwar Koji <camk0966@gmail.com>
2024-11-18 03:03:33 +0800

    Add components to the app

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useRecipeStore } from './recipeStore'
+import { useNavigate } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const { deleteRecipe } = useRecipeStore();
   const history = useHistory();
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/');
 
     history.push('/');
   };
