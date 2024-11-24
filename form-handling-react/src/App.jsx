e22c571ae8732f35ac60b573b5d474dc17a3d047
Mahiamk <mahikomohammed@gmail.com>
2024-11-24 22:21:42 +0800

    initial commit

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,7 @@ import FormikForm from "./components/formikForm";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
       <h1>React Form Handling with Formik</h1>
       <FormikForm />
     </div>
