78baf313d39d1adf81715eefbce5b0421edf6111
Mahiamk <mahikomohammed@gmail.com>
2024-11-24 22:31:27 +0800

    initial commit

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -5,17 +5,15 @@ import * as Yup from "yup";
 const FormikForm = () => {
   // Validation schema using Yup
   const validationSchema = Yup.object({
-    username: Yup.string()
-      .min(3, "Username must be at least 3 characters")
-      .required("Username is required"),
+    username: Yup.string().required("Username is required."),
     email: Yup.string()
       .email("Invalid email address")
-      .required("Email is required"),
+      .required("Email is required."),
     password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .required("Password is required"),
+      .min(6, "Password must be at least 6 characters.")
+      .required("Password is required."),
   });
-
+  
   // Initial form values
   const initialValues = {
     username: "",
