4540a8717ef46e37e82c86961c1fb687e5ec29bc
Anwar Koji <camk0966@gmail.com>
2024-11-26 11:27:13 +0800

    initial commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
