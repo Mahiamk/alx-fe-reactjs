bdab78fb37d475aa88c1c29fd7312a5681b5b707
Anwar Koji <camk0966@gmail.com>
2024-11-26 11:53:45 +0800

    initial commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './components/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
+import BlogPost from './pages/BlogPost';
 
 function App() {
   const isAuthenticated = false; // Simulate authentication status
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/blog/:id" element={<BlogPost />} />
       <Route
         path="/profile/*"
         element={
