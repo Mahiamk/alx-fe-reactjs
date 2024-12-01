15a6e9a2d9e6a9fa2710eacec34d25c75cdfcd9f
Mahiamk <mahikomohammed@gmail.com>
2024-12-01 13:23:22 +0800

    Update UserProfile using TailwindCSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="mx-auto rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="mx-auto rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-lg text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
       <p className="text-sm sm:text-base md:text-xl text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
