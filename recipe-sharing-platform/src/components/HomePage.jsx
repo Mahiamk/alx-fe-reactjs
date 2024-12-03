93f7ebfe83921ed604de5b35dcb5d8dbb8c2ec84
Mahiamk <mahikomohammed@gmail.com>
2024-12-03 16:42:35 +0800

    Add HomePage component to fetch data and display data from data.json

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,7 +13,7 @@ return (
     <h1 className="text-4xl font-bold text-center mb-8">Recipe Sharing Platform</h1>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden">
+        <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-200 teext-black font-bold py-2 px-4 rounded transition duration-300 hover: translate-y-[-5px]">
           <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
           <div className="p-4">
             <h2 className="text-xl font-semibold text-gray-800">{recipe.title}</h2>
