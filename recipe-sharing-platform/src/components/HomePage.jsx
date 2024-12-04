3cd0b3336aae524c28db4bdd58b68faf7665f9ef
Mahiamk <mahikomohammed@gmail.com>
2024-12-04 21:54:43 +0800

    chore: add react router dom for routing

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import  { useState, useEffect } from "react";
 import recipeData from '../data.json'
+import { Link } from "react-router-dom";
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
   useEffect(()=>{
@@ -8,22 +9,32 @@ const HomePage = () => {
     };
     fetchRecipes();
   },[]);
+
 return (
   <div className="p-4 max-w-7xl mx-auto">
     <h1 className="text-4xl font-bold text-center mb-8">Recipe Sharing Platform</h1>
+
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-200 teext-black font-bold py-2 px-4 rounded transition duration-300 hover: translate-y-[-5px]">
-          <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
+        <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-200 text-black font-bold py-2 px-4 rounded transition duration-300 hover:translate-y-[-5px]">
+          <img
+            src={recipe.image}
+            alt={recipe.title}
+            className="w-full h-48 object-cover"
+          />
           <div className="p-4">
             <h2 className="text-xl font-semibold text-gray-800">{recipe.title}</h2>
-            <p className="text-black-bold-600 text-sm mt-2">{recipe.summary}</p>
+            <p className="text-gray-600 text-sm mt-2">{recipe.summary}</p>
           </div>
+          <Link to={`/recipe/${recipe.id}`}className="text-blue-600 mt-4 inline-block hover:underline font-semibold">
+            View Recipe
+          </Link>
         </div>
       ))}
     </div>
   </div>
 );
 };
+
 
 export default HomePage;
--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
-        src="https://via.placeholder.com/150" 
+        src="/Users/anwarmohammedkoji/Downloads/photo_2024-12-01 13.25.33.jpeg"
         alt="User" 
         className="mx-auto rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
